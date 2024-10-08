95ac6a91c8de51a85128f62f619a8bb8a1c7b14c
taha <149532993+taha-hassan21@users.noreply.github.com>
2024-10-08 21:22:36 +0530

    Finished the fourth exercise

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,10 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, ...args) {
+    for (let i = array.length - 1; i >= 0; i--) {
+        if (args.includes(array[i])) {
+            array.splice(i, 1);
+        }
+    }
+    return array;
 };
 
 // Do not edit below this line

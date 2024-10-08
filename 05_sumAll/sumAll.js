c6c9b590e4075ae6b9c4650f1f3a337387e6894f
taha <149532993+taha-hassan21@users.noreply.github.com>
2024-10-08 21:52:27 +0530

    finished the fifth exercise

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,30 @@
-const sumAll = function() {
-
+const sumAll = function(num1, num2) {
+    if (typeof num1!='number' || typeof num2!='number') {
+        return "ERROR";
+    }
+    let round1=Math.floor(num1);
+    let round2=Math.floor(num2);
+    if (num1%round1!=0 || num2%round2!=0) {
+        return "ERROR";
+    }
+    if (num1<0 || num2<0) {
+        return "ERROR";
+    }
+    let min;
+    let max;
+    if (num1>num2) {
+        max=num1;
+        min=num2;
+    }
+    else {
+        max=num2;
+        min=num1;
+    }
+    let sum=0;
+    for (let i=min;i<=max;i++) {
+        sum+=i;
+    }
+    return sum;
 };
 
 // Do not edit below this line

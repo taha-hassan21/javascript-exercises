7691f08fd4cbff10596580427bbe29fdc885cada
taha <149532993+taha-hassan21@users.noreply.github.com>
2024-10-08 23:20:11 +0530

    finishes 10th exercise

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,17 @@
-const fibonacci = function() {
+const fibonacci = function(num) {
+    if (+num<0) {
+        return 'OOPS';
+    }
+    let first=0;
+    let second=1;
+    let ans;
+    for (let i=0;i<+num;i++) {
+        ans=first+second;
+        first=second;
+        second=ans;
+    }
 
+    return first;
 };
 
 // Do not edit below this line

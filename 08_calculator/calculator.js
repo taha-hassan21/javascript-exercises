b9e60d29cce15ec7b56ced4db445f4c41393a062
taha <149532993+taha-hassan21@users.noreply.github.com>
2024-10-08 22:45:04 +0530

    finished eighth

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,40 @@
-const add = function() {
-	
+const add = function(a,b) {
+	return a+b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+	return a-b;
 };
 
-const sum = function() {
-	
+const sum = function(array) {
+	let sum=0;
+  for (i of array) {
+    sum+=i;
+  }
+  return sum;
 };
 
-const multiply = function() {
-
+const multiply = function(array) {
+  let ans=1;
+  for (i of array) {
+    ans*=i;
+  }
+  return ans;
 };
 
-const power = function() {
-	
+const power = function(a, b) {
+	return Math.pow(a,b);
 };
 
-const factorial = function() {
-	
+const factorial = function(n) {
+	if (n===0) {
+    return 1;
+  }
+  let product=1;
+  for (let i=n;i>0;i--) {
+    product*=i;
+  }
+  return product;
 };
 
 // Do not edit below this line

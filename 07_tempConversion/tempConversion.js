77acf2c6d1c9bbb62940c7094127a452563b0252
taha <149532993+taha-hassan21@users.noreply.github.com>
2024-10-08 22:17:17 +0530

    Finished 7 exercise

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,13 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(fan) {
+  let x=(fan-32)*5/9;
+  x=Math.round(x*10)/10;
+  return x;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(cel) {
+  let y=cel*9/5+32
+  y=Math.round(y*10)/10;
+  return y;
 };
 
 // Do not edit below this line

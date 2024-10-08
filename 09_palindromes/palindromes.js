de44fcb42c89c9e7f806921c37c16feeaf10d124
taha <149532993+taha-hassan21@users.noreply.github.com>
2024-10-08 23:02:56 +0530

    finished ninth exercise

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,15 @@
-const palindromes = function () {
+const palindromes = function (string) {
+    const all= 'abcdefghijklmnopqrstuvwxyz0123456789';
 
+    const cleanedString=string
+        .toLowerCase()
+        .split('')
+        .filter((character) => all.includes(character))
+        .join('');
+    
+    const rev=cleanedString.split('').reverse().join('');
+
+    return cleanedString===rev;
 };
 
 // Do not edit below this line

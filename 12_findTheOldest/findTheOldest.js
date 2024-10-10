1535a1f67fe6d9d8f416d171b76693ea203bd8c2
taha <149532993+taha-hassan21@users.noreply.github.com>
2024-10-10 21:51:47 +0530

    Finished the exercise by making a sort function and returning the first index.

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,13 @@
-const findTheOldest = function() {
+const findTheOldest = function(array) {
+    const ordered=array.sort((a,b)=>{
+        const lastPerson=(a.yearOfDeath || new Date().getFullYear())-a.yearOfBirth;
+        const nextPerson=(b.yearOfDeath || new Date().getFullYear())-b.yearOfBirth;
+        return lastPerson>nextPerson?-1:1;
+    })
 
+    return ordered[0];
 };
+
 
 // Do not edit below this line
 module.exports = findTheOldest;
